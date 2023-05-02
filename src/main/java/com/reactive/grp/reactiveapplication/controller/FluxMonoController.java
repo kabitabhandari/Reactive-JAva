9e55530a2811abd8923df21dbf737c359010d9f5
kabita bhandari <36861110+kabitabhandari@users.noreply.github.com>
2023-05-02 17:43:37 -0500

    Simple rest call that returns mono, flux and stream

---
+++ src/main/java/com/reactive/grp/reactiveapplication/controller/FluxMonoController.java
@@ -0,0 +1,31 @@
+package com.reactive.grp.reactiveapplication.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+@RestController
+public class FluxMonoController {
+
+
+    @GetMapping("/flux")
+    public Flux<Integer> flux() {
+        return Flux.just(11, 21, 31)
+                .log();
+    }
+
+    @GetMapping("/mono")
+    public Mono<String> helloWorldMono() {
+        return Mono.just("hello-world-beginners");
+    }
+
+    @GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Long> stream() {
+        return Flux.interval(Duration.ofSeconds(5))
+                .log();
+    }
+}
